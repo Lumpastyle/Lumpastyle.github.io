36e223554bcbca4029ba36acbd83477967fc06a6
Anthony <Lumpastyle@gmail.com>
2017-07-11 22:43:56 +0200

    homepage style

--- app/footer.php
+++ app/footer.php
@@ -1,14 +1,19 @@
+<hr>
 
-<footer class="footer">
+<footer class="footer container-fluid">
     <div class="footer-container">
-        <div class="footer-copyright">
-            © 2016-2017 Anthony Dias
+        <div class="footer-state left">
+            Je suis actuellement à la recherche d'une alternance en tant que <span class="blue">Designer UI/UX</span>.
         </div>
-        <div class="footer-socials">
-            <a class="socials-item" href=""><img src="assets/svg/facebook_white.svg" alt=""></a>
-            <a class="socials-item" href=""><img src="assets/svg/dribbble_white.svg" alt=""></a>
-            <a class="socials-item" href=""><img src="assets/svg/github_white.svg" alt=""></a>
-            <a class="socials-item" href=""><img src="assets/svg/linkedin_white.svg" alt=""></a>
+        <div class="clearfix"></div>
+        <div class="footer-copyright left">
+            © 2016-2017 Anthony Dias - <a class="loud mail" href="mailto:lumpastyle@gmail.com">lumpastyle@gmail.com</a>
+        </div>
+        <div class="footer-socials right">
+            <a class="socials-item" href=""><img src="assets/svg/facebook_black.svg" alt=""></a>
+            <a class="socials-item" href=""><img src="assets/svg/dribbble_black.svg" alt=""></a>
+            <a class="socials-item" href=""><img src="assets/svg/github_black.svg" alt=""></a>
+            <a class="socials-item" href=""><img src="assets/svg/linkedin_black.svg" alt=""></a>
         </div>
     </div>
 </footer>
